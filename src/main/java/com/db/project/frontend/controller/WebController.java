2af843f90846bf0f99ddc884948884e4567ff1e7
Darlitch <errorguy@mail.ru>
2025-06-11 04:51:44 +0700

    Reg, Auth and log out done!

--- src/main/java/com/db/project/frontend/controller/WebController.java
+++ src/main/java/com/db/project/frontend/controller/WebController.java
@@ -1,8 +1,12 @@
 package com.db.project.frontend.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController {
@@ -26,5 +30,13 @@ public class WebController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login?logout=true";
     }
 } 
